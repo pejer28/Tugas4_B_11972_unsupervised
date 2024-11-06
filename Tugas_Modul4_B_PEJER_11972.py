ff69d738a5fa2e270aa9b4d4f5f7e8ff35d258b4
pejer28 <220711972@students.uajy.ac.id>
2024-11-06 15:34:28 +0700

    Update Tugas_Modul4_B_PEJER_11972.py

--- Tugas_Modul4_B_PEJER_11972.py
+++ Tugas_Modul4_B_PEJER_11972.py
@@ -55,9 +55,9 @@ if uploaded_file is not None:
     st.dataframe(input_data)
 #model_path = r'SVR_IPK_model.pkl'
     model_path = {
-        r"AGG_model": os.path.join(model_directory, r'AGG_model.pkl'),
-        r"KMeans_model": os.path.join(model_directory, r'KMeans_model.pkl'),
-        r"DBSCAN_model": os.path.join(model_directory, r'DBSCAN_model.pkl'),
+        "AGG_model": os.path.join(model_directory, r'AGG_model.pkl'),
+        "KMeans_model": os.path.join(model_directory, r'KMeans_model.pkl'),
+        "DBSCAN_model": os.path.join(model_directory, r'DBSCAN_model.pkl'),
     }
 
     # load ketiga model ke dalam dictionary
